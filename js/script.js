731bd30de6df49dfb409bad1fe925cacfdb98d0f
musa001324 <ahmadsambopool@gmail.com>
2026-01-19 11:15:24 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// // function greetings(){
+// //     alert("Welcome User")
+// // }
+// // greetings();
+  
+// function myUser(){
+//     let username= prompt("What Is Your Name ?");
+//     alert("Welcome "+username+"Have a Nice Day Here");
+// }
+// myUser();
+function myfunction(){
+    var x = document.
+    getElementById("mylinks");
+    if(x.style.display === 
+        "block"){
+        x.style.display = "none";
+        }else{
+        x.style.display = "block"        
+        }
+}
